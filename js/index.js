f7afe152319f28be670497db30abd7c8ef99115d
Maria Cassandra <maria.cassandra@gmail.com>
2022-07-21 23:05:48 +0200

    unit tests don't work with my equipment

--- js/index.js
+++ js/index.js
@@ -1,28 +1,32 @@
 // ITERATION 1
-
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
   const price = product.querySelector('.price span').textContent;
   const quantity = product.querySelector('.quantity input').value;
   const subtotal = price * quantity;
+
   product.querySelector(".subtotal span").textContent = subtotal;
   return subtotal;
 }
-  // ITERATION 2
+// ITERATION 2
 function calculateAll() {
-  const products = document.getElementsByClassName("product");
-  updateSubtotal(products[0]);
-  updateSubtotal(products[1]);
+  const singleProduct = document.getElementsByClassName("product");
+  updateSubtotal(singleProduct[0]);
+  updateSubtotal(singleProduct[1]);
+  console.log(singleProduct)
 
-  
-}
-
-
+  let shoppingCart = document.querySelector("#cart");
+  let allProducts = shoppingCart.querySelectorAll(".product");
+  let total = 0;
+  allProducts.forEach((element) => {
+    total += updateSubtotal(element);
+  });
 
 
   // ITERATION 3
-  //... your code goes here
+  document.querySelector('#total-value span').innerText = total.toFixed(2);
+}
 
 
 // ITERATION 4
